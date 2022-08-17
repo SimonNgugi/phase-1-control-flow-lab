c6c4b0f76caedc1e9765e640c21da8d1112f47bc
Simon Ngugi Ngigi <simonngugi88@gmail.com>
2022-08-17 13:53:00 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+if(feet <= 400){
+  return "This one is on me!";
+}
+if(feet > 2500){
+  return "No can do.";
+}
+else if(feet >= 2000){
+  return "I will gladly take your thirty bucks.";
+}
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(NYC){
   // Write your code here!
+  return (NYC === `NYC` ? `Ok, sounds good.` : `No go.`);
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip){
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+    break;
+  }
 }
